4eb824aa4c4cc48c4d026a0c3f00b544687bc122
Avdullahh <gxtrolling@gmail.com>
2026-06-14 10:23:54 +0300

    Remove dead calendar code from date-utils, data-events, and use-categories

--- lib/date-utils.ts
+++ lib/date-utils.ts
@@ -1,8 +1,3 @@
 export function toLocalDateString(d: Date): string {
   return `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')}`;
 }
-
-export function toLocalTimeString(d: Date): string {
-  return `${String(d.getHours()).padStart(2, '0')}:${String(d.getMinutes()).padStart(2, '0')}`;
-}
-
